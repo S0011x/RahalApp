432ed8b972402892dda8400e6f06814373d127e7
Sahar-Yousef <Sell.lla.78@gmail.com>
2024-05-12 12:30:11 +0300

    OneSignal Adding

--- RahalApp/RahalAppApp.swift
+++ RahalApp/RahalAppApp.swift
@@ -6,6 +6,9 @@
 //
 
 import SwiftUI
+import OneSignalFramework
+
+
 
 @main
 struct RahalAppApp: App {
@@ -15,5 +18,28 @@ struct RahalAppApp: App {
         WindowGroup {
             SplashView(isOnboarding: $isOnboarding)
         }
+    }
+}
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+       // Remove this method to stop OneSignal Debugging
+       OneSignal.Debug.setLogLevel(.LL_VERBOSE)
+        
+       // OneSignal initialization
+       OneSignal.initialize("a1f9489d-25d3-4e5f-8f62-7bcc0ef497f4", withLaunchOptions: launchOptions)
+
+       // requestPermission will show the native iOS notification permission prompt.
+       // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+       OneSignal.Notifications.requestPermission({ accepted in
+         print("User accepted notifications: \(accepted)")
+       }, fallbackToSettings: true)
+
+       // Login your customer with externalId
+        OneSignal.login("EXTERNAL_ID")
+
+            
+       return true
     }
 }
