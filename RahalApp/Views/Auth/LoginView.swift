d6e428403aa0aea3375d860410ce73b144d870b8
S0011x <suha.alrajhi01@gmail.com>
2024-05-18 22:42:02 +0300

    login view with function

--- RahalApp/Views/Auth/LoginView.swift
+++ RahalApp/Views/Auth/LoginView.swift
@@ -1,199 +1,152 @@
-//
-//  LoginView.swift
-//  RahalApp
-//
-//  Created by Juman Dhaher on 20/10/1445 AH.
-//
-import AuthenticationServices
 import SwiftUI
-
+import AuthenticationServices
+import CloudKit
 
 struct LoginView: View {
     @ObservedObject var loginViewModel = LoginViewModel()
-    //    @State var appleButton: AppleButtonLogic
-    //        init() {
-    //            self._appleButton = State(initialValue: AppleButtonLogic())
-    //        }
-
-    
+    var User: UserInfo?
     var body: some View {
-        
-          @State  var showingAccountView = loginViewModel.showingAccountView
-          @State  var isSigninigIn = loginViewModel.isSigninigIn
-          
-          @State  var email = loginViewModel.email
-          @State  var password = loginViewModel.password
-          @State  var firstName = loginViewModel.firstName
-          @State  var lastName = loginViewModel.lastName
-
-//        Text("\(isSigninigIn)")
-//        Text("\(showingAccountView)")
-
-            NavigationStack {
-
-            ZStack{
+        NavigationStack {
+            ZStack {
                 Image("img_untitled_artwork").resizable().brightness(-0.4).ignoresSafeArea()
                 
-                ScrollView{
-                    VStack(alignment:.center){
-                        EmailView(email: email)
-                        PasswordView(password: password)
-                        NavigationLink(destination: ForgetPassword()) {
-                            HStack{
-                                Text("نسيت كلمة المرور؟").foregroundColor( Color("WhiteA7001")).fontWeight(.bold)}
-                            .frame(width:300 ,alignment: .trailing)
-                        }
-                        
+                ScrollView {
+                    VStack(alignment: .center) {
+                        emailView(email: $loginViewModel.email)
+                        passwordView(password: $loginViewModel.password)
+
                         Spacer(minLength: 30)
-                        ButtonWidget(text: "تسجيل الدخول")
-                        Spacer(minLength: 20)
-                        NavigationLink(destination: UserSignUp(), label: {
-                            HStack{
-                                Text("سجل معنا").underline(color : Color("tivany"))
-                                    .foregroundColor(Color("tivany"))
-                                Text("ليس لديك حساب؟").foregroundColor( Color("WhiteA7001")).fontWeight(.heavy)
+                        Button(action: {
+                            loginViewModel.fetchUserByEmailAndPassword(email: loginViewModel.email, password: loginViewModel.password) { userInfo in
+                                if let userInfo = userInfo {
+                                    // User was found and returned
+                                    print("User logged in: \(userInfo.firstName) \(userInfo.lastName)")
+                                    loginViewModel.showingAccountView = true
+                                } else {
+                                    // No user found or an error occurred
+                                    print("Invalid credentials or user not found")
+                                }
                             }
+                        }, label: {
+                            ButtonWidget(text: "تسجيل الدخول")
                         })
-                        HStack{
-                            line
-                            Text("أو").foregroundColor(.whiteA7001)
-                            line
-                        }.frame(width: 300)
-                        
-                        SignInWithAppleButton(
-                            
-                            .signIn,
-                            onRequest: loginViewModel.configureAppleSignInRequest,
-                            onCompletion: loginViewModel.handleAppleSignInResult
-                        )
-                        .signInWithAppleButtonStyle(.white) // Set the button style to white
-                        .frame(width: 300, height: 40)
-                        .padding()
-                        .environment(\.locale, .init(identifier: "ar"))
-                        
+                        Spacer(minLength: 20)
+                        NavigationLink(destination: UserSignUp()) {
+                            HStack {
+                                Text("سجل معنا").underline(color: Color("tivany"))
+                                    .foregroundColor(Color("tivany"))
+                                Text("ليس لديك حساب؟").foregroundColor(Color("WhiteA7001")).fontWeight(.heavy)
+                            }
+                        }
                     }
-                }.safeAreaPadding(.top,180)
+                }.safeAreaPadding(.top, 180)
             }
             .ignoresSafeArea()
-            .navigationDestination(isPresented: $showingAccountView) {
-                UserInformationView()
-            }
         }
     }
-    
-    var line: some View {
-        VStack { Divider().background(.whiteA7001) }.padding(10)
+
+    private func emailView(email: Binding<String>) -> some View {
+        VStack(alignment: .trailing) {
+            Text("البريد الالكتروني").foregroundColor(Color("WhiteA7001")).fontWeight(.heavy)
+            TextField("", text: email)
+                .multilineTextAlignment(.trailing)
+                .padding()
+                .frame(width: 300, height: 35)
+                .background(Color("WhiteA7001"))
+                .overlay(RoundedRectangle(cornerRadius: 4).stroke(Color("WhiteA7001")).frame(width: 300, height: 35))
+        }
     }
-    
-    
 
-    
-    
-    
-    
-    
-    
-//    extension LoginView {
-        
-//    func widgetBox(text: String) -> some View{
-        
-         func EmailView (email: String) -> some View{
-
-            @State var email = email
-            
-
-            return VStack(alignment: .trailing){
-                Text("البريد الالكتروني").foregroundColor( Color("WhiteA7001")).fontWeight(.heavy)
-                
-                TextField("", text: $email)
-                    .multilineTextAlignment(.trailing)
-                    .padding()
-                
-                    .frame(width: 300, height: 35)
-                
-                    .background(Color("WhiteA7001"))
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 4)
-                            .stroke(
-                                Color("WhiteA7001"))
-                            .frame(width: 300, height: 35))
-            }
+    private func passwordView(password: Binding<String>) -> some View {
+        VStack(alignment: .trailing) {
+            Text("كلمة المرور").foregroundColor(Color("WhiteA7001")).fontWeight(.heavy)
+            SecureField("", text: password)
+                .multilineTextAlignment(.trailing)
+                .padding()
+                .frame(width: 300, height: 35)
+                .background(Color("WhiteA7001"))
+                .overlay(RoundedRectangle(cornerRadius: 4).stroke(Color("WhiteA7001")).frame(width: 300, height: 35))
         }
-        
-   
-        
-    func PasswordView (password: String) -> some View{
-         
-      
-        @State var password = password
-            
-         return   VStack(alignment: .trailing){
-                Text("كلمة المرور").foregroundColor( Color("WhiteA7001")).fontWeight(.heavy)
-                
-                
-                TextField("", text: $password)
-                    .multilineTextAlignment(.trailing)
-                    .padding()
-                
-                    .frame(width: 300, height: 35)
-                
-                    .background(Color("WhiteA7001"))
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 4)
-                            .stroke(
-                                Color("WhiteA7001"))
-                            .frame(width: 300, height: 35))
-                
-            }
-        }
-        
-//    }
+    }
 }
 
-final class LoginViewModel: NSObject, ObservableObject {
-    
+ class LoginViewModel: ObservableObject {
     @Published var showingAccountView = false
     @Published var isSigninigIn = false
-    
+
     @Published var email = ""
     @Published var password = ""
     @Published var firstName = ""
     @Published var lastName = ""
     
-    //Apple Sign up
-     func configureAppleSignInRequest(request: ASAuthorizationAppleIDRequest) {
+    func configureAppleSignInRequest(request: ASAuthorizationAppleIDRequest) {
         request.requestedScopes = [.fullName, .email]
- 
     }
     
-     func handleAppleSignInResult(result: Result<ASAuthorization, Error>)  {
+    func handleAppleSignInResult(result: Result<ASAuthorization, Error>) {
         switch result {
         case .success(let authResults):
-            if let appleIDCredential = authResults.credential as? ASAuthorizationAppleIDCredential  {
-                
-                 firstName = appleIDCredential.fullName?.givenName ?? ""
-                 lastName = appleIDCredential.fullName?.familyName ?? ""
-                 email = appleIDCredential.email ?? ""
-                
-                showingAccountView = true  // Set this to true to navigate to account
-                isSigninigIn = true
-               
-                print("First❤️ Name: \(firstName)")
-                print("Last Name: \(lastName)")
-                print("Email: \(email)")
-                print("isSigninigIn: \(isSigninigIn)")
-                print("showingAccountView: \(showingAccountView)")
-             
+            guard let appleIDCredential = authResults.credential as? ASAuthorizationAppleIDCredential else {
+                return
             }
-            
-//            showingAccountView = true  // Set this to true to navigate to account
-//            isSigninigIn = true
-            
+            firstName = appleIDCredential.fullName?.givenName ?? ""
+            lastName = appleIDCredential.fullName?.familyName ?? ""
+            email = appleIDCredential.email ?? ""
+            showingAccountView = true
+            isSigninigIn = true
         case .failure(let error):
             print("Authentication error: \(error.localizedDescription)")
-            
-           
         }
     }
     
+     func fetchUserByEmailAndPassword(email: String, password: String, completion: @escaping (UserInfo?) -> Void) {
+         let encryptedPassword = encryptPassword(password)
+         let predicate = NSPredicate(format: "Email == %@ AND encryptedPass == %@", email, encryptedPassword)
+         let query = CKQuery(recordType: "UserRecords", predicate: predicate)
+
+         CKContainer(identifier: "iCloud.com.macrochallange.test.TripManagement").publicCloudDatabase.perform(query, inZoneWith: nil) { records, error in
+             DispatchQueue.main.async {
+                 if let error = error {
+                     print("Failed to fetch user: \(error)")
+                     completion(nil)  // Corrected to return nil on error
+                 } else if let record = records?.first {
+                     // Construct UserInfo from the record if found
+                     let userInfo = UserInfo(
+                         idUser: record.recordID.recordName,
+                         firstName: record["firstName"] as? String ?? "",
+                         lastName: record["lastName"] as? String ?? "",
+                         Email: record["Email"] as? String ?? "",
+                         confirmEmail: record["confirmEmail"] as? String ?? "",
+                         encryptedPass: record["encryptedPass"] as? String ?? "",
+                         confirmPassword: record["confirmPassword"] as? String ?? "",
+                         comingTrips: record["comingTrips"] as? String ?? "",
+                         onTrip: record["onTrip"] as? Bool ?? false,
+                         record: record
+                     )
+                     completion(userInfo)  // Return the found UserInfo
+                 } else {
+                     print("No user found or wrong password.")
+                     completion(nil)  // Corrected to return nil if no user is found
+                 }
+             }
+         }
+     }
+
+    private func encryptPassword(_ password: String) -> String {
+        let data = password.data(using: .utf8)!
+        return data.base64EncodedString()
+    }
+    
+    
+    struct ButtonWidget: View {
+        var text: String
+        
+        var body: some View {
+            Text(text)
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+        }
+    }
 }
