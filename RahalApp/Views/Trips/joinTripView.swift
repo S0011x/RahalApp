b4994b5f667fc0c0becd376cacf86d79480765f8
S0011x <suha.alrajhi01@gmail.com>
2024-05-23 10:34:20 +0300

    joinTriop

---
+++ RahalApp/Views/Trips/joinTripView.swift
@@ -0,0 +1,18 @@
+//
+//  joinTrip.swift
+//  Masier
+//
+//  Created by suha alrajhi on 13/11/1445 AH.
+//
+
+import SwiftUI
+
+struct joinTrip: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    joinTrip()
+}
