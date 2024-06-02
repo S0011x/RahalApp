0d877099f0414b4cc817ba9657ebd8783de5a88b
S0011x <suha.alrajhi01@gmail.com>
2024-06-02 13:52:35 +0300

    final copy

---
+++ RahalApp/Views/Trips/testt.swift
@@ -0,0 +1,18 @@
+//
+//  testt.swift
+//  Masier
+//
+//  Created by suha alrajhi on 19/11/1445 AH.
+//
+
+import SwiftUI
+
+struct testt: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    testt()
+}
